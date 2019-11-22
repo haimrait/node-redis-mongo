e074d64633c7a8291efa8b7bc33a39da346e2fe9
DESKTOP-MOOGR13\haimr <haimrait@gmail.com>
2019-11-22 22:12:34 +0200

    Init express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
