d0020aa55d123e22175d3784a313d78a09b0e482
DESKTOP-MOOGR13\haimr <haimrait@gmail.com>
2019-11-23 08:59:58 +0200

    add ability to get and post books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,32 @@
+const mongoose = require("mongoose");
+const Book = mongoose.model("Book");
+
 module.exports = app => {
-  app.get("/api/books", (req, res) => {
-    res.send("ok");
+  app.get("/api/books", async (req, res) => {
+    let books;
+    if (req.query.author) {
+      books = await Book.find({ author: req.query.author });
+    } else {
+      books = await Book.find();
+    }
+
+    res.send(books);
+  });
+
+  app.post("/api/books", async (req, res) => {
+    const { title, content, author } = req.body;
+
+    const book = new Book({
+      title,
+      content,
+      author
+    });
+
+    try {
+      await book.save();
+      res.send(book);
+    } catch (err) {
+      res.send(400, err);
+    }
   });
 };
